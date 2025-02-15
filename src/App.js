5964f06410b5ad16e215393deb62629456179cb5
Leo Fu <leo.fu@validic.com>
2025-02-15 05:23:02 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in lss than a day
         </a>
       </header>
     </div>
